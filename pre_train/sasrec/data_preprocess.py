dd3bf39a31d5cb9ec8380f256f4c81222069be7f
Soheil Hoseini <soheil.hosseini78@gmail.com>
2025-08-19 19:49:55 +0330

    preprocessing

--- pre_train/sasrec/data_preprocess.py
+++ pre_train/sasrec/data_preprocess.py
@@ -1,282 +1,126 @@
-import sys
-import copy
-import torch
-import random
-import numpy as np
-from collections import defaultdict
-from multiprocessing import Process, Queue
 import os
-from datetime import datetime
-from pytz import timezone
-from torch.utils.data import Dataset
+import os.path
+import gzip
+import json
+import pickle
+from tqdm import tqdm
+from collections import defaultdict
 
 
 def get_project_path():
     """Get the base project path from environment variable or default"""
     return os.environ.get('PROJECT_BASE_PATH', '/content/drive/MyDrive/Rec_Proj_DL')
 
-# sampler for batch generation
-def random_neq(l, r, s):
-    t = np.random.randint(l, r)
-    while t in s:
-        t = np.random.randint(l, r)
-    return t
 
-
-def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
-    def sample():
-
-        user = np.random.randint(1, usernum + 1)
-        while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
-
-        seq = np.zeros([maxlen], dtype=np.int32)
-        pos = np.zeros([maxlen], dtype=np.int32)
-        neg = np.zeros([maxlen], dtype=np.int32)
-        nxt = user_train[user][-1]
-        idx = maxlen - 1
-
-        ts = set(user_train[user])
-        for i in reversed(user_train[user][:-1]):
-            seq[idx] = i
-            pos[idx] = nxt
-            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
-            nxt = i
-            idx -= 1
-            if idx == -1: break
-
-        return (user, seq, pos, neg)
-
-    np.random.seed(SEED)
-    while True:
-        one_batch = []
-        for i in range(batch_size):
-            one_batch.append(sample())
-
-        result_queue.put(zip(*one_batch))
-
-
-class WarpSampler(object):
-    def __init__(self, User, usernum, itemnum, batch_size=64, maxlen=10, n_workers=1):
-        self.result_queue = Queue(maxsize=n_workers * 10)
-        self.processors = []
-        for i in range(n_workers):
-            self.processors.append(
-                Process(target=sample_function, args=(User,
-                                                      usernum,
-                                                      itemnum,
-                                                      batch_size,
-                                                      maxlen,
-                                                      self.result_queue,
-                                                      np.random.randint(2e9)
-                                                      )))
-            self.processors[-1].daemon = True
-            self.processors[-1].start()
-
-    def next_batch(self):
-        return self.result_queue.get()
-
-    def close(self):
-        for p in self.processors:
-            p.terminate()
-            p.join()
-
-# DataSet for ddp
-class SeqDataset(Dataset):
-    def __init__(self, user_train, num_user, num_item, max_len): 
-        self.user_train = user_train
-        self.num_user = num_user
-        self.num_item = num_item
-        self.max_len = max_len
-        print("Initializing with num_user:", num_user)
-
+def parse(path):
+    g = gzip.open(path, 'rb')
+    for l in tqdm(g):
+        yield json.loads(l)
         
-    def __len__(self):
-        return self.num_user
-        
-    def __getitem__(self, idx):
-        user_id = idx + 1
-        seq = np.zeros([self.max_len], dtype=np.int32)
-        pos = np.zeros([self.max_len], dtype=np.int32)
-        neg = np.zeros([self.max_len], dtype=np.int32)
+def preprocess(fname):
+    countU = defaultdict(lambda: 0)
+    countP = defaultdict(lambda: 0)
+    line = 0
+
+    file_path = f'{get_project_path()}/data/amazon/{fname}.json.gz'
     
-        nxt = self.user_train[user_id][-1]
-        length_idx = self.max_len - 1
-        
-        # user의 seq set
-        ts = set(self.user_train[user_id])
-        for i in reversed(self.user_train[user_id][:-1]):
-            seq[length_idx] = i
-            pos[length_idx] = nxt
-            if nxt != 0: neg[length_idx] = random_neq(1, self.num_item + 1, ts)
-            nxt = i
-            length_idx -= 1
-            if length_idx == -1: break
+    # counting interactions for each user and item
+    for l in parse(file_path):
+        line += 1
+        if ('Beauty' in fname) or ('Toys' in fname):
+            if l['overall'] < 3:
+                continue
+        asin = l['asin']
+        rev = l['reviewerID']
+        time = l['unixReviewTime']
 
-        return user_id, seq, pos, neg
-
-class SeqDataset_Inference(Dataset):
-    def __init__(self, user_train, user_valid, user_test,use_user, num_item, max_len): 
-        self.user_train = user_train
-        self.user_valid = user_valid
-        self.user_test = user_test
-        self.num_user = len(use_user)
-        self.num_item = num_item
-        self.max_len = max_len
-        self.use_user = use_user
-        print("Initializing with num_user:", self.num_user)
-
-        
-    def __len__(self):
-        return self.num_user
-        
-    def __getitem__(self, idx):
-        user_id = self.use_user[idx]
-        seq = np.zeros([self.max_len], dtype=np.int32)
-        idx = self.max_len -1
-        seq[idx] = self.user_valid[user_id][0]
-        idx -=1
-        for i in reversed(self.user_train[user_id]):
-            seq[idx] = i
-            idx -=1
-            if idx ==-1: break
-        rated = set(self.user_train[user_id])
-        rated.add(0)
-        pos = self.user_test[user_id][0]
-        neg = []
-        for _ in range(3):
-            t = np.random.randint(1,self.num_item+1)
-            while t in rated: t = np.random.randint(1,self.num_item+1)
-            neg.append(t)
-        neg = np.array(neg)
-        return user_id, seq, pos, neg
-# train/val/test data generation
-def data_partition(fname, path=None):
+        countU[rev] += 1
+        countP[asin] += 1
+    
+    usermap = dict()
     usernum = 0
+    itemmap = dict()
     itemnum = 0
-    User = defaultdict(list)
-    user_train = {}
-    user_valid = {}
-    user_test = {}
-    # assume user/item index starting from 1
+    User = dict()
+    review_dict = {}
+    name_dict = {'title':{}, 'description':{}}
     
-    # f = open('./pre_train/sasrec/data/%s.txt' % fname, 'r')
-    if path == None:
-        f = open(f'{get_project_path()}/data/amazon/%s.txt' % fname, 'r')
-    else:
-        f = open(path, 'r')
-    for line in f:
-        u, i = line.rstrip().split(' ')
-        u = int(u)
-        i = int(i)
-        usernum = max(u, usernum)
-        itemnum = max(i, itemnum)
-        User[u].append(i)
-
-    for user in User:
-        nfeedback = len(User[user])
-        if nfeedback < 3:
-            user_train[user] = User[user]
-            user_valid[user] = []
-            user_test[user] = []
+    f = open(f'{get_project_path()}/data/amazon/meta_{fname}.json', 'r')
+    json_data = f.readlines()
+    f.close()
+    data_list = [json.loads(line[:-1]) for line in json_data]
+    meta_dict = {}
+    for l in data_list:
+        meta_dict[l['asin']] = l
+    
+    for l in parse(file_path):
+        line += 1
+        asin = l['asin']
+        rev = l['reviewerID']
+        time = l['unixReviewTime']
+        
+        threshold = 5
+        if ('Beauty' in fname) or ('Toys' in fname):
+            threshold = 4
+            
+        if countU[rev] < threshold or countP[asin] < threshold:
+            continue
+        
+        if rev in usermap:
+            userid = usermap[rev]
         else:
-            user_train[user] = User[user][:-2]
-            user_valid[user] = []
-            user_valid[user].append(User[user][-2])
-            user_test[user] = []
-            user_test[user].append(User[user][-1])
-    return [user_train, user_valid, user_test, usernum, itemnum]
-
-# TODO: merge evaluate functions for test and val set
-# evaluate on test set
-def evaluate(model, dataset, args):
-    [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
-    NDCG = 0.0
-    HT = 0.0
-    valid_user = 0.0
-
-    if usernum>10000:
-        users = random.sample(range(1, usernum + 1), 10000)
-    else:
-        users = range(1, usernum + 1)
-    for u in users:
-
-        if len(train[u]) < 1 or len(test[u]) < 1: continue
-
-        seq = np.zeros([args.maxlen], dtype=np.int32)
-        idx = args.maxlen - 1
-        seq[idx] = valid[u][0]
-        idx -= 1
-        for i in reversed(train[u]):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1: break
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [test[u][0]]
-        for _ in range(19):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated: t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(*[np.array(l) for l in [[u], [seq], item_idx]])
-        predictions = predictions[0] # - for 1st argsort DESC
-
-        rank = predictions.argsort().argsort()[0].item()
-
-        valid_user += 1
-
-        if rank < 1:
-            NDCG += 1 / np.log2(rank + 2)
-            HT += 1
-        if valid_user % 100 == 0:
-            print('.', end="")
-            sys.stdout.flush()
-
-    return NDCG / valid_user, HT / valid_user
-
-
-# evaluate on val set
-def evaluate_valid(model, dataset, args):
-    [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
-    NDCG = 0.0
-    valid_user = 0.0
-    HT = 0.0
-    if usernum>10000:
-        users = random.sample(range(1, usernum + 1), 10000)
-    else:
-        users = range(1, usernum + 1)
+            usernum += 1
+            userid = usernum
+            usermap[rev] = userid
+            User[userid] = []
         
-    for u in users:
-        if len(train[u]) < 1 or len(valid[u]) < 1: continue
-
-        seq = np.zeros([args.maxlen], dtype=np.int32)
-        idx = args.maxlen - 1
-        for i in reversed(train[u]):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1: break
-
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [valid[u][0]]
+        if asin in itemmap:
+            itemid = itemmap[asin]
+        else:
+            itemnum += 1
+            itemid = itemnum
+            itemmap[asin] = itemid
+        User[userid].append([time, itemid])
         
-        for _ in range(100):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated: t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(*[np.array(l) for l in [[u], [seq], item_idx]])
-        predictions = predictions[0]
-
-        rank = predictions.argsort().argsort()[0].item()
-        valid_user += 1
-
-        if rank < 10:
-            NDCG += 1 / np.log2(rank + 2)
-            HT += 1
-        if valid_user % 100 == 0:
-            print('.', end="")
-            sys.stdout.flush()
-    return NDCG / valid_user, HT / valid_user
+        
+        if itemmap[asin] in review_dict:
+            try:
+                review_dict[itemmap[asin]]['review'][usermap[rev]] = l['reviewText']
+            except:
+                a = 0
+            try:
+                review_dict[itemmap[asin]]['summary'][usermap[rev]] = l['summary']
+            except:
+                a = 0
+        else:
+            review_dict[itemmap[asin]] = {'review': {}, 'summary':{}}
+            try:
+                review_dict[itemmap[asin]]['review'][usermap[rev]] = l['reviewText']
+            except:
+                a = 0
+            try:
+                review_dict[itemmap[asin]]['summary'][usermap[rev]] = l['summary']
+            except:
+                a = 0
+        try:
+            if len(meta_dict[asin]['description']) ==0:
+                name_dict['description'][itemmap[asin]] = 'Empty description'
+            else:
+                name_dict['description'][itemmap[asin]] = meta_dict[asin]['description'][0]
+            name_dict['title'][itemmap[asin]] = meta_dict[asin]['title']
+        except:
+            a =0
+    
+    with open(f'{get_project_path()}/data/amazon/{fname}_text_name_dict.json.gz', 'wb') as tf:
+        pickle.dump(name_dict, tf)
+    
+    for userid in User.keys():
+        User[userid].sort(key=lambda x: x[0])
+        
+    print(usernum, itemnum)
+    
+    f = open(f'{get_project_path()}/data/amazon/{fname}.txt', 'w')
+    for user in User.keys():
+        for i in User[user]:
+            f.write('%d %d\n' % (user, i[1]))
+    f.close()
